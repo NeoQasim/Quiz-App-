367a65aac8e21df8b543fe0236bdbd39b9e7de68
NeoQasim <139628391+NeoQasim@users.noreply.github.com>
2023-08-15 23:52:55 +0500

    completed

---
+++ app.js
@@ -0,0 +1,57 @@
+import boxen from "boxen";
+import chalk from "chalk";
+import inquirer from "inquirer";
+// import showBanner from "node-banner";
+console.log(boxen("boxen working fine"));
+const questions = [
+    {
+        question: "What is the  first letter a,b,c",
+        options: ["a", "b", "c"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "What is the  second letter a,b,c",
+        options: ["a", "b", "c"],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is the  third letter a,b,c",
+        options: ["a", "b", "c"],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "What is the  fourth letter a,b,c,d",
+        options: ["a", "b", "c", "d"],
+        correctAnswerIndex: 3
+    }
+];
+async function quiz() {
+    const welcome = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter your name ",
+            default: "user"
+        }
+    ]);
+    console.log(chalk.blue(`welcome  ${welcome.name} ! `, chalk.green("lets start quiz ")));
+    let score = 0;
+    for (const question of questions) {
+        const { userAnswer } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "userAnswer",
+                message: question.question,
+                choices: question.options
+            }
+        ]);
+        if (question.correctAnswerIndex === question.options.indexOf(userAnswer)) {
+            console.log(chalk.bgGreen('Correct!\n'));
+            score++;
+        }
+        else {
+            console.log(chalk.bgRed.white('Incorrect.\n'));
+        }
+    }
+}
+quiz();
---
+++ app.ts
@@ -0,0 +1,72 @@
+import boxen from "boxen";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+// import showBanner from "node-banner";
+
+console.log(boxen("boxen working fine"));
+
+interface questions {
+    question: string;
+    options: string[];
+    correctAnswerIndex: number;
+}
+
+const questions: questions[] = [
+    {
+        question: "What is the  first letter a,b,c",
+        options: ["a", "b", "c"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "What is the  second letter a,b,c",
+        options: ["a", "b", "c"],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is the  third letter a,b,c",
+        options: ["a", "b", "c"],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "What is the  fourth letter a,b,c,d",
+        options: ["a", "b", "c","d"],
+        correctAnswerIndex: 3
+    }
+];
+
+async function quiz() {
+    const welcome = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter your name ",
+            default: "user"
+        }
+    ])
+
+    console.log(chalk.blue(`welcome  ${welcome.name} ! `, chalk.green("lets start quiz ")));
+
+
+    let score = 0
+    for (const question of questions) {
+        const { userAnswer } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "userAnswer",
+                message: question.question,
+                choices: question.options
+            }
+        ]);
+        if (question.correctAnswerIndex === question.options.indexOf(userAnswer)) {
+            console.log(chalk.bgGreen('Correct!\n'));
+            score++;
+        } else {
+            console.log(chalk.bgRed.white('Incorrect.\n'));
+        }
+    }
+}
+
+
+
+quiz()
